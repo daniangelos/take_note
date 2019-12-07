4ede1268823b21934cadf5fe8ca13e2465d97249
Daniella Angelos <daniaangelos@gmail.com>
2019-12-07 00:24:32 -0300

    checkpoint #1

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,59 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MyNotes());
+
+class MyNotes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Note Taking', home: NoteList());
+  }
+}
+
+class NoteList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return NoteListState();
+  }
+}
+
+class NoteListState extends State<NoteList> {
+  List<String> _notesTaken = [];
+
+  Widget _buildNotesList() {
+    return new ListView.builder(
+      itemBuilder: (context, index) {
+        if (index < _notesTaken.length) {
+          return _buildNoteItem(_notesTaken[index]);
+        }
+      },
+    );
+  }
+
+  Widget _buildNoteItem(String note) {
+    return new ListTile(
+      title: Text(note),
+    );
+  }
+
+  void _addNote() {
+    setState(() {
+      int index = _notesTaken.length;
+      _notesTaken.add('This is note ' + index.toString());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Notes!'),
+      ),
+      body: _buildNotesList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addNote,
+        tooltip: 'Add new note',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:take_note/main.dart';
 
+void main() {
+  testWidgets('Finds the app title', (WidgetTester tester) async {
+    await tester.pumpWidget(MyNotes());
+    expect(find.text('My Notes!'), findsOneWidget);
+  });
+
+  testWidgets('Add a note after click', (WidgetTester tester) async {
+    await tester.pumpWidget(MyNotes());
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    expect(find.text('This is note 0'), findsOneWidget);
+  });
+}
